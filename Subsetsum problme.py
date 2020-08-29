e14dfb98017a7e8ccce6e247fcfef32f4f379562
keerthi4600 <65659798+keerthi4600@users.noreply.github.com>
2020-08-29 18:07:25 +0530

    Create Subsetsum problme.py

---
+++ Subsetsum problme.py
@@ -0,0 +1,22 @@
+def isSubsetSum(set, n, sum): 
+    subset =([[False for i in range(sum + 1)] for i in range(n + 1)]) 
+    for i in range(n + 1): 
+        subset[i][0] = True
+    for i in range(1, sum + 1): 
+        subset[0][i]= False
+    for i in range(1, n + 1): 
+        for j in range(1, sum + 1): 
+            if j<set[i-1]: 
+                subset[i][j] = subset[i-1][j] 
+            if j>= set[i-1]: 
+                subset[i][j] = (subset[i-1][j] or subset[i - 1][j-set[i-1]]) 
+    return subset[n][sum] 
+t=int(input())
+for i in range(t):
+    n,k=map(int,input().split())
+    lst=list(map(int,input().strip().split(' ')))[:n]
+    if (isSubsetSum(lst, n, k) == True): 
+        print("YES") 
+    else: 
+        print("NO") 
+      
